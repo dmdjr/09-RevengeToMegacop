13e7a856da9678cf3ae76ef9c7f450cf0aaeb896
ahn eun seong <tjddmsdks@gmail.com>
2026-02-17 22:47:32 +0900

    머신건 추가

--- Assets/Scripts/Weapon/Gun/HandGun.cs
+++ Assets/Scripts/Weapon/Gun/HandGun.cs
@@ -1,12 +1,4 @@
 public class HandGun : GunWeapon
 {
-    protected override void Use()
-    {
-        if (CanFire()) Fire();
-    }
-
-    private void Fire()
-    {
-        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-    }
+    
 }
--- Assets/Scripts/Weapon/base/GunWeapon.cs
+++ Assets/Scripts/Weapon/base/GunWeapon.cs
@@ -9,15 +9,25 @@ public abstract class GunWeapon : Weapon
     [SerializeField] protected GameObject bulletPrefab;
 
     [SerializeField] protected Transform firePoint;
-    
+
     protected override void Awake()
     {
         base.Awake();
         if (MaxAmmo < Ammo) Ammo = MaxAmmo;
     }
 
+    protected override void Use()
+    {
+        if (CanFire()) Fire();
+    }
+    
     protected bool CanFire()
     {
         return 0 < Ammo;
+    }
+
+    private void Fire()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 }
